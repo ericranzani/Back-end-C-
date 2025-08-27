92c18899c880426c1cbdb5945961d48534ba0c9c
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-08-27 12:24:07 -0300

    modulo inicial para apresentação de conceitos básicos

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fundamentos.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Eric";
+p.Idade = 30;
+p.Apresentar();
