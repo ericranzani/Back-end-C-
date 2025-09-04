5eb8b60f4451aa9c9ef687f14611829f3879ff26
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-09-04 11:00:11 -0300

    atualização

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,13 +1,4 @@
-﻿using Fundamentos.Models;
-
-DateTime dataAtual = DateTime.Now;
-Console.WriteLine(dataAtual);
+﻿
 
 
 
-
-// Pessoa p = new Pessoa();
-
-// p.Nome = "Eric";
-// p.Idade = 30;
-// p.Apresentar();
--- Fundamentos/obj/Debug/net9.0/Fundamentos.AssemblyInfo.cs
+++ Fundamentos/obj/Debug/net9.0/Fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ac17c61c73e2a1ca12a4b6f1c5d1da70a62afc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9709cfb1b5b536e644ceca3b3a30e5839b0f6ecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
